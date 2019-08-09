fa3b0d713dbb937a2ea49ace222ff25d671c18e4
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-09 15:11:06 +0200

    add and config routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+
+import Home from './pages/Home.js';
+import AppList from './pages/AppList.js';
+import NotFound from './pages/NotFound.js';
 
 import 'milligram';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/apps' exact component={AppList} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
